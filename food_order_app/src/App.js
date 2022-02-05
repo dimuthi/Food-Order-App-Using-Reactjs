aeef06a49165945d0f61d5173c48d903dbba0a7c
dimuthi tharaka <dimuthi@gmail.com>
2022-02-05 15:34:37 +0530

    meal items and meal summary created

--- food_order_app/src/App.js
+++ food_order_app/src/App.js
@@ -1,11 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Meals from "./components/Meals/Meals";
 import Header from "./components/Layout/Header";
 import { Fragment } from "react";
+
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
