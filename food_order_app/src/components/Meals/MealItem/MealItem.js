ec2360c2844521e751291fdec6f509639c43c9db
dimuthi tharaka <dimuthi@gmail.com>
2022-02-05 17:26:14 +0530

    added some styles to meal items

---
+++ food_order_app/src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,16 @@
+import classes from "./MealItem.module.css";
+
+const MealItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+  return (
+    <li className={classes.meals}>
+      <div>
+        <h1>{props.name}</h1>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div></div>
+    </li>
+  );
+};
+export default MealItem;
